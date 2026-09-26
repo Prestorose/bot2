fa0cd35abbd3f95edfc787fc990522ae9df66a0c
twelfth <145135548+Prestorose@users.noreply.github.com>
2026-09-25 22:51:32 -0400

    Update init.js

--- events/ready/init.js
+++ events/ready/init.js
@@ -1,18 +1,13 @@
 const mongoose = require("mongoose");
-const noblox = require("noblox.js");
 const { ActivityType } = require("discord.js");
-require("dotenv").config();
 
 module.exports = async (client) => {
     console.log(`✅ | ${client.user.tag}`);
 
     client.user.setPresence({
-        activities: [{ name: "Krestia", type: ActivityType.Watching }],
+        activities: [{ name: "Northside APM RP", type: ActivityType.Watching }],
     });
 
     await mongoose.connect(process.env.MONGOURL);
     console.log("✅ | Connected to DB");
-
-    const user = await noblox.setCookie(process.env.COOKIE);
-    console.log(`✅ | Logged in as ${user.name}`);
 };
